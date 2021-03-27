b529ebb9918a7ccb30922d8422e7d0392e718c41
Michael Pingel <mdpingel@gmail.com>
2021-03-27 15:27:48 -0700

    update treats controller to return to details page when adding or deleting a flavor

--- PierreTreats/Controllers/TreatsController.cs
+++ PierreTreats/Controllers/TreatsController.cs
@@ -111,16 +111,17 @@ namespace PierreTreats.Controllers
         _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId });
       }
       _db.SaveChanges();
-      return RedirectToAction("Index");
+      return RedirectToAction("Details", "Treats", new { id = treat.TreatId });
     }
 
     [HttpPost]
     public ActionResult DeleteFlavor(int joinId)
     {
       FlavorTreat joinEntry = _db.FlavorTreat.FirstOrDefault(entry => entry.FlavorTreatId == joinId);
+      int treatId = joinEntry.TreatId;
       _db.FlavorTreat.Remove(joinEntry);
       _db.SaveChanges();
-      return RedirectToAction("Index");
+      return RedirectToAction("Details", "Treats", new { id = treatId });
     }
   }
 }
