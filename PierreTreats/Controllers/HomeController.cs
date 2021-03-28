8a833285a22e7ee04b2122cd258af543e535c29f
Michael Pingel <mdpingel@gmail.com>
2021-03-27 19:03:05 -0700

    add functionality to view all treats and their flavors on the home index

--- PierreTreats/Controllers/HomeController.cs
+++ PierreTreats/Controllers/HomeController.cs
@@ -1,12 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using PierreTreats.Models;
 
 namespace PierreTreats.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly PierreTreatsContext _db;
+
+    public HomeController(PierreTreatsContext db)
+    {
+      _db = db;
+    }
+
     public ActionResult Index()
     {
-      return View();
+      List<Treat> model = _db.Treats.ToList();
+      return View(model);
     }
   }
 }
